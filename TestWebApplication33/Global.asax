189630a27517408d4310669a72d01ec8b7a88c8d
Joseph Mathew <joseph.mathew@gsa.gov>
2017-08-04 15:15:14 -0400

    Initial Commit for multiproj31 repo.

---
+++ TestWebApplication33/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="TestWebApplication33.Global_asax" Language="vb" %>
---
+++ TestWebApplication33/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("TestWebApplication33")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("GSA")>
+<Assembly: AssemblyProduct("TestWebApplication33")>
+<Assembly: AssemblyCopyright("Copyright © GSA 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("164fb7b7-7473-44f8-8034-7a471dad2b36")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
